5d4f39f3f5059963a46dd3b54dfebaa681865337
MohammedRasinK <mohammdrasink@gmail.com>
2026-01-04 17:10:18 +0530

    MONGODB ATLAS CONNECTED

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/miniolxDB').then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   try {
     console.log('Connected to MongoDB');
   } catch (error) {

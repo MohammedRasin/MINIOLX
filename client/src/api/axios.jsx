bece2f091e51915e8e83631a95898d77574d32f7
MohammedRasinK <mohammdrasink@gmail.com>
2026-01-04 18:07:35 +0530

    render setup

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000/api',
+  baseURL: import.meta.env.VITE_BACKEND_API || 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use(req => {
